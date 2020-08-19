6bab1c85dd050918e0566244b16b493a084184c2
Ajay Kancherlapalli <46937665+ajaykan@users.noreply.github.com>
2020-08-19 10:02:02 -0700

    Add files via upload

--- short_put.py
+++ short_put.py
@@ -1,19 +1,37 @@
 import yfinance as yf
-
+import datetime
 
 basket = ["AAPL", "MSFT", "FB", "NVDA", "AMD", "V"]
 
-tickers = []
+ticker_objs = []
 for i in basket:
-    tickers.append(yf.Ticker(i))
+    ticker_objs.append(yf.Ticker(i))
 
-# exp = aapl.options
+exp = ticker_objs[0].options
 
-# options = aapl.option_chain(exp[3]) # option chain for 9/10/2020
+options = ticker_objs[0].option_chain(exp[3]) # option chain for 9/10/2020
 
 # hist = aapl.history(period='3mo')
 
-def range(ticker, months):
-    return
+def price_range(ticker_obj, weeks): # returns tuple (min, max, no. months)
+    time = str(weeks) + "wk"
+    hist = ticker_obj.history(period=time)
+    high = max(hist['High'])
+    low = min(hist['Low'])
+    return (low, high, weeks)
 
-print(tickers)
+print(price_range(ticker_objs[0], 6))
+
+def price_by_date(ticker_obj, date):
+    today = datetime.date.today()
+    if type(date) is str:
+        date = str_to_date(date)
+    delta = today - date
+    return delta
+
+def str_to_date(date):
+    date = date.split("-")
+    date = list(map(int, date))
+    return datetime.date(date[0], date[1], date[2])
+
+print(price_by_date(ticker_objs[0], exp[3]))
