6cc7fc070c5427d62a7a5150df5a0aaa320e5ca9
Ajay Kancherlapalli <46937665+ajaykan@users.noreply.github.com>
2020-08-19 10:47:01 -0700

    Add files via upload

--- short_put.py
+++ short_put.py
@@ -1,37 +1,57 @@
 import yfinance as yf
 import datetime
 
-basket = ["AAPL", "MSFT", "FB", "NVDA", "AMD", "V"]
+basket = ["AMD", "AMZN", "FB", "GOOG", "DIS", "JPM"]
 
 ticker_objs = []
 for i in basket:
     ticker_objs.append(yf.Ticker(i))
 
+amd = ticker_objs[0]
+amzn = ticker_objs[1]
+fb = ticker_objs[2]
+goog = ticker_objs[3]
+dis = ticker_objs[4]
+jpm = ticker_objs[5]
+
 exp = ticker_objs[0].options
 
 options = ticker_objs[0].option_chain(exp[3]) # option chain for 9/10/2020
 
-# hist = aapl.history(period='3mo')
 
-def price_range(ticker_obj, weeks): # returns tuple (min, max, no. months)
-    time = str(weeks) + "wk"
-    hist = ticker_obj.history(period=time)
-    high = max(hist['High'])
-    low = min(hist['Low'])
-    return (low, high, weeks)
-
-print(price_range(ticker_objs[0], 6))
-
-def price_by_date(ticker_obj, date):
-    today = datetime.date.today()
-    if type(date) is str:
-        date = str_to_date(date)
-    delta = today - date
-    return delta
+# UTILITY
 
 def str_to_date(date):
     date = date.split("-")
     date = list(map(int, date))
     return datetime.date(date[0], date[1], date[2])
 
-print(price_by_date(ticker_objs[0], exp[3]))
+# END
+
+
+def price_range(ticker_obj, days): # returns tuple (min, max, days)
+    time = str(days) + "days"
+    hist = ticker_obj.history(period=time)
+    high = max(hist['High'])
+    low = min(hist['Low'])
+    return (low, high, days)
+
+def price_by_date(ticker_obj, date, time="Close"): # time: {Open, High, Low, Close}
+    today = datetime.date.today()
+    if type(date) is str:
+        date = str_to_date(date)
+    delta = (today - date).days + 2
+    if delta < 0:
+        return "Invalid date"
+    delta = str(delta) + "d"    
+    hist = ticker_obj.history(period=delta)
+    try:
+        hist = hist.loc[date]
+        return hist[time]
+    except:
+        return "Non trading day"
+
+
+past_date = datetime.date(2020, 10, 15)
+
+print(price_by_date(amd, past_date))
