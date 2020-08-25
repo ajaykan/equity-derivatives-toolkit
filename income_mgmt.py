a4ddfb963203a0f8285159095ada2da8f9afb7ba
Ajay Kancherlapalli <46937665+ajaykan@users.noreply.github.com>
2020-08-24 17:34:34 -0700

    portfolio income

--- income_mgmt.py
+++ income_mgmt.py
@@ -1,6 +1,31 @@
-from classes import Position
+from classes import Portfolio, Position
 
 import yfinance as yf
 
 
-k = Position(yf.Ticker("AMD"))
+def position_income(pos):
+    assert(isinstance(pos, Position))
+
+    share_count = pos.share_count
+    annual_div = pos.annual_div
+
+    return annual_div * share_count
+
+
+def createPortfolio(share_dict): # takes in dict: {ticker (str): (num shares (int), avg cost (float)}, returns Portfolio obj
+    positions = []
+    for ticker, data in share_dict.items():
+        pos = Position(yf.Ticker(ticker)) # create position with ticker
+        pos.add_position(data[0], data[1])
+
+
+aapl_div = 3.04
+k = Position(yf.Ticker("AAPL"))
+k.annual_div = aapl_div
+
+k.add_position(150, 300)
+
+print(position_income(k))
+
+
+
