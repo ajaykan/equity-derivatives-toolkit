0adf15d675ea4caed85deae3dd090a0a73581e67
Ajay Kancherlapalli <46937665+ajaykan@users.noreply.github.com>
2020-10-18 20:53:49 -0700

    Add files via upload

--- option_tracker.py
+++ option_tracker.py
@@ -1,4 +1,5 @@
 from classes import Option
+import utils
 import yfinance as yf
 import datetime as dt
 
@@ -9,3 +10,10 @@ fb = yf.Ticker("FB")
 
 leap = Option(fb, sample_date_future, 290, True)
 print(leap)
+
+class Option_Tracker():
+
+    def __init__(self, option):
+        # assert(type(option) == Option)
+        self.date_init = utils.date_to_str(dt.date.today)
+        self.last_price = 0
